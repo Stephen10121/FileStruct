b6c823eb984ac26ff0013bf5c452248d8ae69b20
Stephen Gruzin <stephengrzn@gmail.com>
2022-05-16 14:32:38 -0700

    Made a get directory function that returns the directory in json

---
+++ backend/dirGet.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs');
+
+async function getTFiles(directoryPath) {
+    let allFiles = {
+        G_files: []
+    }
+    const files = await fs.promises.readdir(directoryPath, { withFileTypes: true });
+    for (const item of files) {
+        if (item.isDirectory()) {
+            allFiles[item.name] = await getFiles(`${directoryPath}/${item.name}`)
+        } else {
+            allFiles.G_files.push(item.name);
+        }
+    }
+    return allFiles;
+}
+
+async function getFiles(directoryPath) {
+    fs.access(directoryPath, (error) => {
+        if (error) {
+            fs.mkdir(directoryPath, { recursive: true }, (err) => {
+                if (err) {
+                    throw err;
+                }
+                return getTFiles(directoryPath);
+            });
+        }
+    });
+    return getTFiles(directoryPath);
+}
