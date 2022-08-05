ac8eb10139e12c70dd0685a40db21b3b9d388ad3
Stephen Gruzin <stephengrzn@gmail.com>
2022-08-05 14:14:18 -0700

    Forgot to make paths linux compatible

--- backend/server.js
+++ backend/server.js
@@ -63,13 +63,13 @@ const io = socketio(server, {
 });
 
 app.get("/profile", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..\\public\\index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 app.get("/shared", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..\\public\\index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 app.get("/logout", (_req, res) => {
-  res.sendFile(path.join(__dirname, "..\\public\\index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.post("/upload", async (req, res) => {
